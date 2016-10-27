259ba9ab587bdd14d4b6c92771e50a6ea9db099c
Michael Melatti <melatti.michael@gmail.com>
2016-10-27 14:14:01 -0400

    add ss to makefile...
    also added update to makefile also declared SS_PROC_NR

--- include/minix/com.h
+++ include/minix/com.h
@@ -81,6 +81,7 @@
 #define VM_PROC_NR   ((endpoint_t) 8)   /* memory server */
 #define PFS_PROC_NR  ((endpoint_t) 9)  /* pipe filesystem */
 #define SCHED_PROC_NR ((endpoint_t) 10)	/* scheduler */
+#define SS_PROC_NR ((endpoint_t) 11) /* Semaphore proc by Michael Melatti */
 #define LAST_SPECIAL_PROC_NR	11	/* An untyped version for
                                            computation in macros.*/
 #define INIT_PROC_NR ((endpoint_t) LAST_SPECIAL_PROC_NR)  /* init
